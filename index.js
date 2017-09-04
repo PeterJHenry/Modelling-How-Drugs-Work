eb25ad03ddba7492353e5bc62cb69e6d4f6d682f
staktika <jason_kai@outlook.com>
2017-09-04 22:23:04 +0800

    Graphing now works, and ligand C and D are changeable

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ const logK = [
     ['', '', '', '', '']
 ];
 
+const colorTable = [
+    'rgb(255, 153, 51)', // orange
+    'rgb(255, 80, 80)', // red
+    'rgb(51, 204, 255)', // light blue
+    'rgb(0, 204, 0)', // green
+    'rgb(153, 102, 51)', // brown
+    'rgb(153, 51, 255)'];// purple
+
 // keeps the count of checked boxes
 var subTypeCheckedCount = 0;
 
@@ -30,33 +38,58 @@ const state = {
         ['', '', '', '', ''],
         ['', '', '', '', ''],
         ['', '', '', '', '']
-    ]
+    ],
+    activeLigandRow: [false, false, false, false, false, false]
 };
 
 
 // TODO: use functional programming.
 
 
-// <---->
 // Autofills the ligands according to the selected
-// TODO  --- make it less dirty
 // TODO -- change select receptor 12th index
-function ligandAutoFill(e, rowIndex) {
+function ligandAutoFill(selectedLigand, rowIndex) {
+    updateDOM();
     var table = document.getElementById('ligandTable').children[0];
 
     for (var i = 0; i < 5; i++) {
-        table.children[rowIndex].children[i + 1].children[0].value = logK[e.value][i];
-        state.ligands[rowIndex][i] = logK[e.value][i];
-        table.children[rowIndex].children[i + 1].children[0].disabled = (e.value < 10 || e.value == 12);
+        table.children[rowIndex].children[i + 1].children[0].value = logK[selectedLigand.value][i];
+        state.ligands[rowIndex][i] = logK[selectedLigand.value][i];
+        table.children[rowIndex].children[i + 1].children[0].disabled = (selectedLigand.value < 10 || selectedLigand.value == 12);
+        //
+        // if(selectedLigand.value < 10 || selectedLigand.value == 12){
+        //     table.children[rowIndex].children[i + 1].children[0].disabled = true;
+        // } else {
+        //     table.children[rowIndex].children[i + 1].children[0].disabled = false;
+        //     // table.children[rowIndex].children[i + 1].children[0].onblur = updateCustomValue();
+        // }
     }
+
+    state.activeLigandRow[rowIndex] = selectedLigand !== 12;
+    plotGraph();
+
+
 }
 
+function updateCustomValue(rowIndex) {
+    console.log(rowIndex);
+    var table = document.getElementById('ligandTable').children[0];
+    var row = [];
+    for (var i = 0; i < 5; i++) {
+        row.push(parseInt(table.children[rowIndex].children[i + 1].children[0].value));
+    }
+    state.ligands[rowIndex] = row;
+    plotGraph();
+}
 // Updates graph and data, call this function when DOM updates needed
 // this function also updates state
 // currently not yet in use
 function updateDOM() {
     var ligandTable = document.getElementById('ligandTable').children[0];
-    for (var y = 0; y < 6; y++) for (var x = 0; x < 5; x++) state.ligand[y][x] = ligandTable.children[y].children[x + 1].children[0].value
+
+    //copy ligand values to state
+    // for (var y = 0; y < 6; y++) for (var x = 0; x < 5; x++) state.ligands[y][x] = ligandTable.children[y].children[x + 1].children[0].value;
+    plotGraph();
 
 
 }
@@ -120,8 +153,6 @@ function validateRelDensityRow(currentCellNumber) {
         if (state.subTypePresent[i]) currentTotal = currentTotal + state.relDensity[i];
     }
 
-    console.log(currentTotal);
-
     if (currentTotal == 0) {
         currentCell.value = 100;
         state.relDensity[currentCellNumber] = 100;
@@ -136,16 +167,37 @@ function validateRelDensityRow(currentCellNumber) {
 
 
 // TODO -fix graph
+// TODO -  this only works with one graph
 function plotGraph(logValue) {
 
-    // temporary
-    var dataSet = exportData(9.3);
-    var graph = {
-        x: dataSet[0],
-        y: dataSet[1],
-        mode: 'lines'
+    var subtypeIndex = function () {
+        var arrayTrue = [];
+        for (var i = 0; i < 5; i++) if (state.subTypePresent[i]) arrayTrue.push(i);
+        return arrayTrue;
     };
-    // temporary
+
+
+
+    var data = [];
+
+    if (subtypeIndex().length === 1) {
+        var activeColumn = subtypeIndex()[0];
+        for (var i = 0; i < 6; i++) {
+            if (state.activeLigandRow[i]) {
+                var dataSet = exportData(state.ligands[i][activeColumn]);
+                var graph = {
+                    x: dataSet[0],
+                    y: dataSet[1],
+                    mode: 'lines',
+                    line: {
+                        color: colorTable[i],
+                        width: 1
+                    }
+                };
+                data.push(graph);
+            }
+        }
+    }
     var forceY = {
         x: [-11, -2],
         y: [0, 0],
@@ -154,17 +206,15 @@ function plotGraph(logValue) {
             width: 1
         }
     };
+    data.push(forceY);
 
-    var data = [graph, forceY];
-
-
+    // Plotly.purge('myDiv', data);
     Plotly.newPlot('myDiv', data);
 }
 
 
 function exportData(logValue) {
     var STEP = 0.1;
-
     var dataSet = [[], []];
 
     for (var x = -11; x < -2; x = x + STEP) {
@@ -176,7 +226,6 @@ function exportData(logValue) {
 
     }
 
-    console.log(dataSet);
     return dataSet;
 
 }
@@ -184,3 +233,50 @@ function exportData(logValue) {
 function oneDataCal(x, logVal) {
     return 100 / (1 + Math.pow(10, x + logVal))
 }
+
+
+//Chart JS
+// function plotGraph() {
+//
+//     var aDatasets1 = [1, 2, 3, 4, 5];
+//     var aDatasets2 = [5, 6, 7, 87, 89, 9];
+//     var ctx = document.getElementById("myChart");
+//     var myChart = new Chart(ctx, {
+//         type: 'line',
+//         data: {
+//             labels: [-11,-10,-9,-8,-7,-6,-5,-4,-3,-2],
+//             datasets: [
+//                 {
+//                     label: "Data1",
+//                     //fill:false,
+//                     // fillColor: "rgba(0,0,0,0)",
+//                     strokeColor: "rgba(220,220,220,1)",
+//                     pointColor: "rgba(200,122,20,1)",
+//
+//                     data: aDatasets1
+//                 },
+//                 {
+//                     label: "Data2",
+//                     // fillColor: 'rgba(0,0,0,0)',
+//                     strokeColor: 'rgba(220,180,0,1)',
+//                     pointColor: 'rgba(220,180,0,1)',
+//                     data: aDatasets2
+//                 }
+//
+//
+//             ]
+//         },
+//         options: {
+//             scales: {
+//                 yAxes: [{
+//                     display: true,
+//                     ticks: {
+//                         suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
+//                         // OR //
+//                         beginAtZero: true   // minimum value will be 0.
+//                     }
+//                 }]
+//             }
+//         }
+//     });
+// }
