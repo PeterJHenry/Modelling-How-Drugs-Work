3f43af1ba74065223381255d77a5cbebb5223f11
mddykng <21493664@student.uwa.edu.au>
2017-10-10 11:20:15 +0800

    instructions

--- js/index.js
+++ js/index.js
@@ -9,3 +9,18 @@ $("ul.list-inline").find("a").click(function (e) {
         scrollTop: $(section).offset().top
     });
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
+}
