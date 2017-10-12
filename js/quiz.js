6972aba0392f2aeafa1ca68f8d66ae47269f05f6
mddykng <21493664@student.uwa.edu.au>
2017-10-13 01:01:48 +0800

    quiz update

--- js/quiz.js
+++ js/quiz.js
@@ -10,10 +10,13 @@ var subtypeIndex;
 var subtypePercentage;
 var ligandIndexes;
 var subtypeAnswers = [];
-var studentAnswers;
+var inputAnswers = [];
+
+var subTypeCheckedCount;
 
 $(document).ready(function () {
     setQuizProperties();
+    addReceptorListener();
     $('.quizAnswers').hide();
 });
 
@@ -36,6 +39,14 @@ function randomiseSubType() {
         }
         subtypePercentage[1] = 100 - subtypePercentage[0];
     }
+    if(subtypePercentage[1] == null){
+      subtypeIndex[0]++;
+      subtypeIndex[1] = null;
+    }
+    else {
+      subtypeIndex[0]++;
+      subtypeIndex[1]++;
+    }
     subtypeAnswers = subtypeAnswers.concat([subtypeIndex,subtypePercentage]);
 }
 
@@ -51,11 +62,6 @@ function randomiseLigand() {
     }
   }
 }
-
-function storeAnswer() {
-
-}
-
 
 function setQuizProperties() {
     $('#cover-questionLength').html(numberOfQuestions);
@@ -116,11 +122,6 @@ function get_dataset(ligandIndex) {
 function redrawGraph() {
 	// Generate data to pass to the graph.
     var data = [];
-
-    // console.log("Subtype Indexes: " + subtypeIndex);
-    // console.log("Subtype Percentages: " + subtypePercentage);
-    // console.log("Ligands: " + ligandIndexes);
-
     for (var i = 0; i < 5; i++) {
         ligandIndex = ligandIndexes[i]
         var dataSet = get_dataset(ligandIndex);
@@ -146,12 +147,105 @@ function generateQuestion() {
     redrawGraph();
 }
 
+function checkAnswers(){
+  if($('input[type=checkbox]:checked').length <= 0){
+    alert('Please select an answer');
+  }
+  else {
+    storeAnswers();
+    checkEnd();
+  }
+}
+
+function clearValues(){
+  $('input[type="checkbox"]:checked').prop('checked',false);
+  $('input[type="number"]').val('');
+}
+
+function storeAnswers() {
+  var subtypes = [];
+  var percentage = [];
+
+  $('input[type=checkbox]:checked').each(function(){
+    subtypes.push(parseInt($(this).val()));
+  });
+
+  $('input[type=number]').each(function(){
+    if($(this).val()!=""){
+      percentage.push(parseInt($(this).val()));
+    }
+  });
+
+  if(subtypes.length === 1){
+    subtypes[1] = null;
+    percentage[1] = null;
+  }
+
+  inputAnswers = inputAnswers.concat([subtypes,percentage]);
+  //clearValues();
+}
+
 function renderResults() {
-  $('#a1').html("M"+(subtypeAnswers[0][0]+1)+" "+subtypeAnswers[1][0]+"%, M"+(subtypeAnswers[0][1]+1)+" "+subtypeAnswers[1][1]+"%");
-  $('#a2').html("M"+(subtypeAnswers[2][0]+1)+" "+subtypeAnswers[3][0]+"%, M"+(subtypeAnswers[2][1]+1)+" "+subtypeAnswers[3][1]+"%");
-  $('#a3').html("M"+(subtypeAnswers[4][0]+1)+" "+subtypeAnswers[5][0]+"%, M"+(subtypeAnswers[4][1]+1)+" "+subtypeAnswers[5][1]+"%");
-  $('#a4').html("M"+(subtypeAnswers[6][0]+1)+" "+subtypeAnswers[7][0]+"%, M"+(subtypeAnswers[6][1]+1)+" "+subtypeAnswers[7][1]+"%");
-  $('#a5').html("M"+(subtypeAnswers[8][0]+1)+" "+subtypeAnswers[9][0]+"%, M"+(subtypeAnswers[8][1]+1)+" "+subtypeAnswers[9][1]+"%");
+  var score = 0;
+
+  for(var i = 0; i < 10; i+=2){
+    console.log(inputAnswers[i][0],subtypeAnswers[i][0]);
+    console.log(inputAnswers[i][1],subtypeAnswers[i][1]);
+    if((inputAnswers[i][0] === subtypeAnswers[i][0]) && (inputAnswers[i][1] === subtypeAnswers[i][1]) || (inputAnswers[i][0] === subtypeAnswers[i][1]) && (inputAnswers[i][1] === subtypeAnswers[i][0])){
+      score++;
+    }
+  }
+
+  if(score===0){
+    $('#score').html(score+"/5... Better get studying...");
+  }
+  else if (score===1){
+    $('#score').html(+score+"/5.. At least you got one!");
+  }
+  else if(score===2){
+    $('#score').html(+score+"/5.. So close!");
+  }
+  else if(score===3){
+    $('#score').html(+score+"/5.. You passed! Just..");
+  }
+  else if(score===4){
+    $('#score').html(+score+"/5! Congratulations");
+  }
+  else if(score===5){
+    $('#score').html(+score+"/5! Whoa! We got a genius here!");
+  }
+
+  //STUDENT ANSWERS
+  if(inputAnswers[0][1]===null) $('#q1').html("M"+inputAnswers[0][0]+" "+inputAnswers[1][0]+"%");
+  else $('#q1').html("M"+inputAnswers[0][0]+" "+inputAnswers[1][0]+"%<br>M"+inputAnswers[0][1]+" "+inputAnswers[1][1]+"%");
+
+  if(inputAnswers[2][1]===null) $('#q2').html("M"+inputAnswers[2][0]+" "+inputAnswers[3][0]+"%");
+  else $('#q2').html("M"+inputAnswers[2][0]+" "+inputAnswers[3][0]+"%<br>M"+inputAnswers[2][1]+" "+inputAnswers[3][1]+"%");
+
+  if(inputAnswers[4][1]===null) $('#q3').html("M"+inputAnswers[4][0]+" "+inputAnswers[5][0]+"%");
+  else $('#q3').html("M"+inputAnswers[4][0]+" "+inputAnswers[5][0]+"%<br>M"+inputAnswers[4][1]+" "+inputAnswers[5][1]+"%");
+
+  if(inputAnswers[6][1]===null) $('#q4').html("M"+inputAnswers[6][0]+" "+inputAnswers[7][0]+"%");
+  else $('#q4').html("M"+inputAnswers[6][0]+" "+inputAnswers[7][0]+"%<br>M"+inputAnswers[6][1]+" "+inputAnswers[7][1]+"%");
+
+  if(inputAnswers[8][1]===null) $('#q5').html("M"+inputAnswers[8][0]+" "+inputAnswers[9][0]+"%");
+  else $('#q5').html("M"+inputAnswers[8][0]+" "+inputAnswers[9][0]+"%<br>M"+inputAnswers[8][1]+" "+inputAnswers[9][1]+"%");
+
+  //ANSWERS
+  if(subtypeAnswers[0][1]===null) $('#a1').html("M"+subtypeAnswers[0][0]+" "+subtypeAnswers[1][0]+"%");
+  else $('#a1').html("M"+subtypeAnswers[0][0]+" "+subtypeAnswers[1][0]+"%<br>M"+subtypeAnswers[0][1]+" "+subtypeAnswers[1][1]+"%");
+
+  if(subtypeAnswers[2][1]===null) $('#a2').html("M"+subtypeAnswers[2][0]+" "+subtypeAnswers[3][0]+"%");
+  else $('#a2').html("M"+subtypeAnswers[2][0]+" "+subtypeAnswers[3][0]+"%<br>M"+subtypeAnswers[2][1]+" "+subtypeAnswers[3][1]+"%");
+
+  if(subtypeAnswers[4][1]===null) $('#a3').html("M"+subtypeAnswers[4][0]+" "+subtypeAnswers[5][0]+"%");
+  else $('#a3').html("M"+subtypeAnswers[4][0]+" "+subtypeAnswers[5][0]+"%<br>M"+subtypeAnswers[4][1]+" "+subtypeAnswers[5][1]+"%");
+
+  if(subtypeAnswers[6][1]===null) $('#a4').html("M"+subtypeAnswers[6][0]+" "+subtypeAnswers[7][0]+"%");
+  else $('#a4').html("M"+subtypeAnswers[6][0]+" "+subtypeAnswers[7][0]+"%<br>M"+subtypeAnswers[6][1]+" "+subtypeAnswers[7][1]+"%");
+
+  if(subtypeAnswers[8][1]===null) $('#a5').html("M"+subtypeAnswers[8][0]+" "+subtypeAnswers[9][0]+"%");
+  else $('#a5').html("M"+subtypeAnswers[8][0]+" "+subtypeAnswers[9][0]+"%<br>M"+subtypeAnswers[8][1]+" "+subtypeAnswers[9][1]+"%");
 }
 
 function endQuiz(){
@@ -171,6 +265,7 @@ function checkEnd() {
     generateQuestion();
   }
   else if(currentNumber > numberOfQuestions){
+    storeAnswers();
     endQuiz();
   }
   else {
@@ -188,18 +283,6 @@ function collectUserInput(){
         userAnswer.push(parseInt(value));
     });
     userAnswerStack.push(userAnswer);
-}
-
-// Convert random ratios to questions and push them into stack
-function convertDataToQuestion_userInputRatio() {
-
-}
-
-
-function countCorrectAnswers() {
-    var correctCount = 0;
-    for (var i = 0; i < numberOfQuestions; i++) if (userAnswerStack[0] === correctAnswerStack[0]) correctCount++;
-    return correctCount;
 }
 
 //// Check if the box can be checked
@@ -283,30 +366,10 @@ function validateRelDensityRow(currentCellNumber) {
     var currentCellValue = parseInt(receptorRelDenTableCell(currentCellNumber).value);
     if (currentCellValue < 0) currentCellValue = receptorRelDenTableCell(currentCellNumber).value = 0;
 
+    if(currentCellValue > 80) currentCellValue = receptorRelDenTableCell(currentCellNumber).value = 80;
+    if(currentCellValue < 20) currentCellValue = receptorRelDenTableCell(currentCellNumber).value = 20;
     if (currentCellNumber === previousCheckedBox0) receptorRelDenTableCell(previousCheckedBox1).value = 100 - currentCellValue;
     else receptorRelDenTableCell(previousCheckedBox0).value = 100 - currentCellValue;
-}
-
-function validateLigandValue() {
-    for (var j = 0; j < 6; j++) {
-        for (var i = 1; i < 6; i++) {
-            if (parseInt(ligandTableCell(i, j).value) > 10) {
-                ligandTableCell(i, j).value = 10;
-            } else if (parseInt(ligandTableCell(i, j).value) < 3) {
-                ligandTableCell(i, j).value = 3;
-            }
-        }
-    }
-}
-
-function addLigandListener() {
-    $('.ligandInput').blur(function () {
-        validateLigandValue();
-    }).keyup(function () {
-        validateLigandValue();
-    }).change(function () {
-        validateLigandValue();
-    });
 }
 
 function addReceptorListener() {
@@ -318,13 +381,16 @@ function addReceptorListener() {
                 validateIndividualCell(count - 1);
             }).change(function () {
                 validateIndividualCell(count - 1);
+            }).keyup(function () {
+                validateIndividualCell(count - 1);
             });
         }
     });
 }
 
 function showBody() {
-    $('body').fadeIn();
+    $('section').fadeIn();
+    $('.loading').fadeOut();
 }
 
 setTimeout(showBody, 5000);
