a6e5231124694906cf450a27f78c863e13b9a441
Robbie <fernandezrobert97@gmail.com>
2017-10-18 19:02:06 +0800

    Added Ligand custom value test

--- js/ligand_tests.js
+++ js/ligand_tests.js
@@ -24,9 +24,42 @@ function test_ligand_values() {
 	console.log("Test passed!");
 }
 
+function test_custom_ligands() {
+	// Test that custom ligands don't accept values outside the range of 3-10.
+	// This takes a long time to run.
+	console.log("Testing custom ligand values....")
+	// For each dropdown element
+	$('.leftInput > select').each(function(i, obj) {
+		// For each custom ligand.
+		for(var i=10; i<12; i++) {
+			$(obj).val(i);
+			$(obj).change(); // Manually fire change event.
+			// For each value input 
+			$(obj).parent().parent().siblings().each(function(j, obj) {
+				// Assert values below 3 are rejected
+				for (var i=2; i>-2; i--) {
+					$(obj).children().first().val(i);
+					$(obj).children().first().change(); // Manually fire change event.
+					value = $(obj).children().first().val();
+					assert(value == 3, value + " is less than 3.");
+				}
+				// Assert values above 10 are rejected 
+				for (var i=11; i<15; i++) {
+					$(obj).children().first().val(i);
+					$(obj).children().first().change(); // Manually fire change event.
+					value = $(obj).children().first().val();
+					assert(value == 10, value + " is greater than 10.");
+				}
+			});
+		}
+	});
+	console.log("Test passed!");
+}
+
 function run_all_tests() {
 	try {
 		test_ligand_values();
+		test_custom_ligands();
 	} catch(Error) {
 		console.log("Test failed");
 		throw Error;
