a924599d408f4a06d8d604cd25bf69c996b85a17
mddykng <21493664@student.uwa.edu.au>
2017-12-14 16:21:56 +0800

    subtype randomisation

--- js/quiz.js
+++ js/quiz.js
@@ -87,9 +87,12 @@ function randomiseSubType() {
             subtypePercentage[0] = 100;
         } else {
             subtypeIndex[1] = Math.floor((Math.random() * 4));
-            if (subtypeIndex[1] === subtypeIndex[0]) {
-                subtypeIndex[0] = subtypeIndex[0] + 1 % 5;
+            while (subtypeIndex[1] === subtypeIndex[0]) {
+              subtypeIndex[1] = Math.floor((Math.random() * 4));
             }
+            // if (subtypeIndex[1] === subtypeIndex[0]) {
+            //     subtypeIndex[0] = subtypeIndex[0] + 1 % 5;
+            // }
             subtypePercentage[1] = 100 - subtypePercentage[0];
         }
         if (subtypePercentage[1] == null) {
